774d709b70459ece3e1cbe57f3b0e7b9537f0f23
ztc0320 <ztc0320@naver.com>
2021-08-09 14:42:18 +0900

    0809 header 작업 완료

--- script/script.js
+++ script/script.js
@@ -8,4 +8,12 @@ $(function(){
         $(this).children("ul").stop().hide();
         $("div#subMenuBG").css({"display" : "none"});
     });
+
+    // 헤더 영역 Lnb 메뉴 하이라이트 효과
+    $("#headerLnb a").mouseover(function(){
+        $(this).siblings("span").css({"visibility" : "visible"});
+    });
+    $("#headerLnb a").mouseout(function(){
+        $(this).siblings("span").css({"visibility" : "hidden"});
+    });
 });
